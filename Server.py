15dae4f6b64fb477f44b6f53706d0d9d4698c42f
Federico Filipponi <federico_filipponi@yahoo.it>
2022-07-25 11:46:35 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -9,6 +9,7 @@ from Crypto.Cipher import AES
 
 font1=("Helvetica", 14)
 font2=("Helvetica", 12)
+SIZE = 512
 
 
 class felpa_server():
@@ -22,12 +23,11 @@ class felpa_server():
         self.conn_clients = []
         self.password_hash = password_hash
         self.window_menu = window_menu
-        self.color = ['lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
-                  'lavender blush', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
-                  'light slate gray', 'gray', 'light gray', 'midnight blue', 'navy', 'cornflower blue',
+        self.color = ['lemon chiffon', 'azure', 'alice blue', 'misty rose', 'dark slate gray', 'dim gray', 'slate gray',
+                  'light slate gray', 'gray', 'midnight blue', 'navy', 'cornflower blue',
                   'dark slate blue',
                   'slate blue', 'medium slate blue', 'light slate blue', 'medium blue', 'royal blue', 'blue',
-                  'dodger blue', 'deep sky blue', 'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
+                  'sky blue', 'light sky blue', 'steel blue', 'light steel blue',
                   'light blue', 'powder blue', 'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise',
                   'cyan', 'light cyan', 'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green',
                   'dark olive green',
@@ -35,17 +35,14 @@ class felpa_server():
                   'lawn green', 'medium spring green', 'green yellow', 'lime green', 'yellow green',
                   'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod', 'light goldenrod yellow',
                   'light yellow', 'yellow', 'gold', 'light goldenrod', 'goldenrod', 'dark goldenrod', 'rosy brown',
-                  'indian red', 'saddle brown', 'sandy brown',
+                  'saddle brown', 'sandy brown',
                   'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange',
-                  'coral', 'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink', 'light pink',
-                  'pale violet red', 'maroon', 'medium violet red', 'violet red',
+                  'coral', 'light coral', 'tomato', 'orange red', 'hot pink', 'deep pink', 'pink', 'light pink',
+                  'maroon', 'medium violet red',
                   'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
-                  'thistle', 'snow2', 'snow3',
-                  'snow4', 'seashell2', 'seashell3', 'seashell4',
+                  'thistle', 'snow4', 'seashell2', 'seashell3', 'seashell4',
                   'LemonChiffon2', 'LemonChiffon3', 'LemonChiffon4', 'cornsilk2', 'cornsilk3',
-                  'cornsilk4', 'ivory2', 'ivory3', 'ivory4', 'honeydew2', 'honeydew3', 'honeydew4',
-                  'LavenderBlush2', 'LavenderBlush3', 'LavenderBlush4', 'MistyRose2', 'MistyRose3',
-                  'MistyRose4', 'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
+                  'azure2', 'azure3', 'azure4', 'SlateBlue1', 'SlateBlue2', 'SlateBlue3',
                   'SlateBlue4', 'RoyalBlue1', 'RoyalBlue2', 'RoyalBlue3', 'RoyalBlue4', 'blue2', 'blue4',
                   'DodgerBlue2', 'DodgerBlue3', 'DodgerBlue4', 'SteelBlue1', 'SteelBlue2',
                   'SteelBlue3', 'SteelBlue4', 'DeepSkyBlue2', 'DeepSkyBlue3', 'DeepSkyBlue4',
@@ -67,35 +64,22 @@ class felpa_server():
                   'LightYellow2', 'LightYellow3', 'LightYellow4', 'yellow2', 'yellow3', 'yellow4',
                   'gold2', 'gold3', 'gold4', 'goldenrod1', 'goldenrod2', 'goldenrod3', 'goldenrod4',
                   'DarkGoldenrod1', 'DarkGoldenrod2', 'DarkGoldenrod3', 'DarkGoldenrod4',
-                  'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'IndianRed1', 'IndianRed2',
-                  'IndianRed3', 'IndianRed4', 'sienna1', 'sienna2', 'sienna3', 'sienna4', 'burlywood1',
-                  'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4', 'tan1',
-                  'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
+                  'RosyBrown1', 'RosyBrown2', 'RosyBrown3', 'RosyBrown4', 'sienna1', 'sienna2', 'sienna3', 'sienna4',
+                  'burlywood1', 'burlywood2', 'burlywood3', 'burlywood4', 'wheat1', 'wheat2', 'wheat3', 'wheat4',
+                  'tan1', 'tan2', 'tan4', 'chocolate1', 'chocolate2', 'chocolate3', 'firebrick1', 'firebrick2',
                   'firebrick3', 'firebrick4', 'brown1', 'brown2', 'brown3', 'brown4', 'salmon1', 'salmon2',
                   'salmon3', 'salmon4', 'LightSalmon2', 'LightSalmon3', 'LightSalmon4', 'orange2',
                   'orange3', 'orange4', 'DarkOrange1', 'DarkOrange2', 'DarkOrange3', 'DarkOrange4',
-                  'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4', 'OrangeRed2',
-                  'OrangeRed3', 'OrangeRed4', 'red2', 'red3', 'red4', 'DeepPink2', 'DeepPink3', 'DeepPink4',
-                  'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
-                  'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4', 'PaleVioletRed1',
-                  'PaleVioletRed2', 'PaleVioletRed3', 'PaleVioletRed4', 'maroon1', 'maroon2',
-                  'maroon3', 'maroon4', 'VioletRed1', 'VioletRed2', 'VioletRed3', 'VioletRed4',
+                  'coral1', 'coral2', 'coral3', 'coral4', 'tomato2', 'tomato3', 'tomato4','DeepPink2', 'DeepPink3',
+                  'DeepPink4', 'HotPink1', 'HotPink2', 'HotPink3', 'HotPink4', 'pink1', 'pink2', 'pink3', 'pink4',
+                  'LightPink1', 'LightPink2', 'LightPink3', 'LightPink4',
                   'magenta2', 'magenta3', 'magenta4', 'orchid1', 'orchid2', 'orchid3', 'orchid4', 'plum1',
                   'plum2', 'plum3', 'plum4', 'MediumOrchid1', 'MediumOrchid2', 'MediumOrchid3',
                   'MediumOrchid4', 'DarkOrchid1', 'DarkOrchid2', 'DarkOrchid3', 'DarkOrchid4',
                   'purple1', 'purple2', 'purple3', 'purple4', 'MediumPurple1', 'MediumPurple2',
                   'MediumPurple3', 'MediumPurple4', 'thistle1', 'thistle2', 'thistle3', 'thistle4',
-                  'grey1', 'grey2', 'grey3', 'grey4', 'grey5', 'grey6', 'grey7', 'grey8', 'grey9', 'grey10',
-                  'grey11', 'grey12', 'grey13', 'grey14', 'grey15', 'grey16', 'grey17', 'grey18', 'grey19',
-                  'grey20', 'grey21', 'grey22', 'grey23', 'grey24', 'grey25', 'grey26', 'grey27', 'grey28',
-                  'grey29', 'grey30', 'grey31', 'grey32', 'grey33', 'grey34', 'grey35', 'grey36', 'grey37',
-                  'grey38', 'grey39', 'grey40', 'grey42', 'grey43', 'grey44', 'grey45', 'grey46', 'grey47',
-                  'grey48', 'grey49', 'grey50', 'grey51', 'grey52', 'grey53', 'grey54', 'grey55', 'grey56',
-                  'grey57', 'grey58', 'grey59', 'grey60', 'grey61', 'grey62', 'grey63', 'grey64', 'grey65',
-                  'grey66', 'grey67', 'grey68', 'grey69', 'grey70', 'grey71', 'grey72', 'grey73', 'grey74',
-                  'grey75', 'grey76', 'grey77', 'grey78', 'grey79', 'grey80', 'grey81', 'grey82', 'grey83',
-                  'grey84', 'grey85', 'grey86', 'grey87', 'grey88', 'grey89', 'grey90', 'grey91', 'grey92',
-                  'grey93', 'grey94', 'grey95', 'grey97', 'grey98', 'grey99']
+                  'grey1', 'grey2', 'grey3', 'grey4', 'grey5']
+
 
     def popup(self, str):
         layout_popup = [[sg.Text(str)], [sg.Button("Exit", key="Exit", size=(13, 1), button_color="orange")]]
@@ -181,12 +165,12 @@ class felpa_server():
         username = ""
         color = ""
         try:
-            password = self.dec(conn.recv(1024))
+            password = self.dec(conn.recv(SIZE))
             if password != self.password_hash.decode("latin-1"):  # Step 1 check if password is correct
                 conn.sendall(self.enc("[DENIED]"))
                 return
             conn.sendall(self.enc("[GRANTED]"))
-            username = self.dec(conn.recv(1024))
+            username = self.dec(conn.recv(SIZE))
             if username in self.username_a:
                 conn.sendall(self.enc("[ERROR_NAME]"))
                 return
@@ -196,11 +180,11 @@ class felpa_server():
             i = 0
             for user in self.username_a[:-1]:
                 conn.sendall(self.enc(f"{user}[SEP]{self.user_color_a[i]}"))
-                conn.recv(1024)
+                conn.recv(SIZE)
                 i += 1
             conn.sendall(self.enc("[END]"))
 
-            conn.recv(1024)
+            conn.recv(SIZE)
 
             rnd = random.randint(0, len(self.color) - 1)
             color = self.color[rnd]
@@ -208,7 +192,7 @@ class felpa_server():
             self.color.remove(color)
             conn.sendall(self.enc(f"{color}"))
 
-            conn.recv(1024)
+            conn.recv(SIZE)
 
         except SocketError as e:
             print(e)
@@ -218,23 +202,21 @@ class felpa_server():
             self.user_color_a.remove(color)
             self.color.append(color)
             self.dimension += 1
-            window_send["TextBox"].print(f"[-] {username} disconnected from FelpaChat.[SEP]{color}",
+            window_send["TextBox"].print(f"{username} disconnected from FelpaChat[SEP]{color}",
                                          background_color='Orange', end='')
             window_send["TextBox"].print('\n', end='')
             self.user_list_update(window_send)
-            client_msg = f"{username} disconnected from FelpaChat."
-            self.broadcast(f"[-] {client_msg}")
+            self.broadcast(f"{username} disconnected from FelpaChat")
             playsound.playsound("error.wav")
             return
-        window_send["TextBox"].print(f"[+] {username} connected to FelpaChat!",
-                                     background_color='SeaGreen1', end='')
-        window_send["TextBox"].print('\n', end='')
+        window_send["TextBox"].print(f"{username} connected to FelpaChat", text_color="green")
+        #window_send["TextBox"].print('\n', end='')
         self.user_list_update(window_send)
-        self.broadcast(f"[+] {username} joined FelpaChat![SEP]{color}")
+        self.broadcast(f"{username} connected to FelpaChat[SEP]{color}")
         playsound.playsound("incoming.wav")
         while True:
             try:
-                client_msg = self.dec(conn.recv(1024))
+                client_msg = self.dec(conn.recv(SIZE))
             except SocketError as e:
                 print(e)
                 conn.close()
@@ -243,23 +225,21 @@ class felpa_server():
                 self.user_color_a.remove(color)
                 self.color.append(color)
                 self.dimension += 1
-                window_send["TextBox"].print(f"[-] {username} disconnected from FelpaChat.[SEP]{color}",
+                window_send["TextBox"].print(f"{username} disconnected from FelpaChat[SEP]{color}",
                                              background_color='Orange', end='')
                 window_send["TextBox"].print('\n', end='')
                 self.user_list_update(window_send)
-                client_msg = f"{username} disconnected from FelpaChat."
-                self.broadcast(f"[-] {client_msg}")
+                self.broadcast(f"{username} disconnected from FelpaChat")
                 playsound.playsound("error.wav")
                 break
             if client_msg == "[QUIT]":
-                client_msg = f"{username} disconnected from FelpaChat.[SEP]{color}"
-                self.broadcast(f"[-] {client_msg}")
+                self.broadcast(f"{username} disconnected from FelpaChat[SEP]{color}")
                 self.conn_clients.remove(conn)
                 self.username_a.remove(username)
                 self.user_color_a.remove(color)
                 self.color.append(color)
                 self.dimension += 1
-                window_send["TextBox"].print(f"[-] {username} disconnected to FelpaChat!", background_color='Orange', end='')
+                window_send["TextBox"].print(f"{username} disconnected to FelpaChat", text_color='Orange', end='')
                 window_send["TextBox"].print('\n', end='')
                 self.user_list_update(window_send)
                 playsound.playsound("error.wav", False)
@@ -290,8 +270,7 @@ class felpa_server():
             update_t = threading.Thread(target=self.update, args=(s, window_send,), daemon=True)
             update_t.start()
             window_send.TKroot.focus_force()
-            window_send["TextBox"].print(f"[+] Server started on port {self.port}, waiting for connections.", background_color='SeaGreen1', end='')
-            window_send["TextBox"].print('\n', end='')
+            window_send["TextBox"].print(f"Server started on port {self.port}, waiting for connections", text_color='blue')
             window_send["ListBox"].print(f"[{self.username}] ", text_color="red")
             playsound.playsound("connected.wav", False)
             while (True):
@@ -315,8 +294,8 @@ class felpa_server():
                     if "[SEP]" in msg:
                         #window_send["TextBox"].print("Cannot send message with keyword '[SEP]'.")
                         self.popup("Cannot send message with keyword '[SEP]'.")
-                    elif len(msg) > 250:
-                        self.popup("Maximun message length is 250 character.")
+                    elif len(msg) > 300:
+                        self.popup("Maximun message length is 300 character.")
                     elif len(msg) != 0:
                         window_send["TextBox"].print(f"[{self.username}]", text_color="red", end='')
                         window_send["TextBox"].print(": " + msg)
