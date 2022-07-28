b5dae9d310238f30cf64ba2991526cf2aed714fc
Felpami <federico_filipponi@yahoo.it>
2022-07-28 09:43:31 +0200

    Added emoji and fix

--- core/emoji_felpa.py
+++ core/emoji_felpa.py
@@ -9,8 +9,12 @@ def emoji_converter(msg):
         return msg.replace("Dx", ":persevering_face:")
     elif ":)" in msg:
         return msg.replace(":)", ":slightly_smiling_face:")
+    elif "(:" in msg:
+        return msg.replace("(:", ":slightly_smiling_face:")
     elif ":(" in msg:
         return msg.replace(":(", ":slightly_frowning_face:")
+    elif "):" in msg:
+        return msg.replace("):", ":slightly_frowning_face:")
     elif "._." in msg:
         return msg.replace("._.", ":neutral_face:")
     elif "-_-" in msg:
@@ -19,8 +23,12 @@ def emoji_converter(msg):
         return msg.replace("q.q", ":loudly_crying_face:")
     elif "Q.Q" in msg:
         return msg.replace("Q.Q", ":loudly_crying_face:")
+    elif "Q_Q" in msg:
+        return msg.replace("Q_Q", ":loudly_crying_face:")
     elif "<3" in msg:
         return msg.replace("<3", ":red_heart:")
+    elif "UwU" in msg:
+        return msg.replace("UwU", ":smiling_face_with_heart-eyes:")
     else:
         return msg
 
